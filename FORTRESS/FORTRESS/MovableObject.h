7ad59cf0acc78443e623a20567f40499758e0aab
tang-ka <gur0907@hanyang.ac.kr>
2025-03-04 22:42:01 +0900

    Update: Add class
    MovableObject, Player, Missile

---
+++ FORTRESS/FORTRESS/MovableObject.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "GameObject.h"
+#include <DirectXMath.h>
+
+class MovableObject :
+    public GameObject
+{
+public:
+    DirectX::XMFLOAT2 position;
+    DirectX::XMFLOAT2 rotation;
+
+    MovableObject() : position(0.0f, 0.0f);
+};
+
