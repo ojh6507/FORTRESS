c2fa9adc33238be32cd21269a641dcc1ab56cbe8
jiwon1304 <jiwon1304@gmail.com>
2025-03-05 01:36:22 +0900

    normal 구하는 함수 추가

--- FORTRESS/FORTRESS/Terrain.h
+++ FORTRESS/FORTRESS/Terrain.h
@@ -14,18 +14,23 @@ public:
     
     void SetOffset(float x, float y);
     
-    bool GetTile(unsigned int x, unsigned int y);
-    void SetTile(unsigned int x, unsigned int y, bool state);
+    bool GetValue(unsigned int x, unsigned int y);
+    void SetValue(unsigned int x, unsigned int y, bool state);
     
+    float GetNormal(unsigned int x, unsigned int y, unsigned int scanInterval, unsigned int numRay = 1);
+
+
     bool CheckCollisionCircle(float _posX, float _posY, float _radius);
     bool CheckCollisionAABB(float _minX, float _minY, float _maxX, float _maxY, float _posX, float _posY);
-
 
 private:
     bool* Map;
     unsigned int Width;
     unsigned int Height;
     float Scale;
+
+    unsigned int GetUpperGround(unsigned int x, unsigned int y) const;
+    unsigned int IsSurface(unsigned int x, unsigned y) const;
 };
 
 
