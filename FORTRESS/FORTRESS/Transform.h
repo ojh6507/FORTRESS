9f948385320438229f181f4f4c12ae29b91a710a
tgds03 <xyzuct6800@naver.com>
2025-03-04 21:51:59 +0900

    transform

--- FORTRESS/FORTRESS/Transform.h
+++ FORTRESS/FORTRESS/Transform.h
@@ -4,7 +4,15 @@ public:
 	FVector3 GetPosition() { return _position; }
 	void SetPosition(FVector3 position) { _position = position; }
 
+	float GetRotation() { return _rotation; }
+	void SetRotation(float rotation) { _rotation = rotation; }
+
+	FVector3 GetScale() { return _scale; }
+	void SetScale(FVector3 scale) { _scale = scale; }
+
 private:
 	FVector3 _position;
+	float _rotation;
+	FVector3 _scale;
 };
 
