94a5634ae40cdcccc0966403f0196ad8137b5833
tgds03 <xyzuct6800@naver.com>
2025-03-05 14:21:51 +0900

    gameobject에 vertex 초기화 제거
    상속 오브젝트가 vertex 초기화하도록...

--- FORTRESS/FORTRESS/GameObject.cpp
+++ FORTRESS/FORTRESS/GameObject.cpp
@@ -3,55 +3,55 @@
 
 GameObject::GameObject(ID3D11Device* device, ID3D11DeviceContext* deviceContext): _device(device), _deviceContext(deviceContext) {
 
-	_vertices.resize(4);
+	//_vertices.resize(4);
 
-	_vertices[0].x = -40.1f;
-	_vertices[0].y = 40.1f;
-	_vertices[0].z = 40.f;
-	_vertices[0].r = 1.f;
-	_vertices[0].g = 1.f;
-	_vertices[0].b = 1.f;
-	_vertices[0].a = 1.f;
+	//_vertices[0].x = -40.1f;
+	//_vertices[0].y = 40.1f;
+	//_vertices[0].z = 40.f;
+	//_vertices[0].r = 1.f;
+	//_vertices[0].g = 1.f;
+	//_vertices[0].b = 1.f;
+	//_vertices[0].a = 1.f;
 
-	_vertices[1].x = 40.1f;
-	_vertices[1].y = 40.1f;
-	_vertices[1].z = 40.f;
-	_vertices[1].r = 1.f;
-	_vertices[1].g = 1.f;
-	_vertices[1].b = 1.f;
-	_vertices[1].a = 1.f;
+	//_vertices[1].x = 40.1f;
+	//_vertices[1].y = 40.1f;
+	//_vertices[1].z = 40.f;
+	//_vertices[1].r = 1.f;
+	//_vertices[1].g = 1.f;
+	//_vertices[1].b = 1.f;
+	//_vertices[1].a = 1.f;
 
-	_vertices[2].x = 40.1f;
-	_vertices[2].y = -40.1f;
-	_vertices[2].z = 40.f;
-	_vertices[2].r = 1.f;
-	_vertices[2].g = 1.f;
-	_vertices[2].b = 1.f;
-	_vertices[2].a = 1.f;
+	//_vertices[2].x = 40.1f;
+	//_vertices[2].y = -40.1f;
+	//_vertices[2].z = 40.f;
+	//_vertices[2].r = 1.f;
+	//_vertices[2].g = 1.f;
+	//_vertices[2].b = 1.f;
+	//_vertices[2].a = 1.f;
 
-	_vertices[3].x = -40.1f;
-	_vertices[3].y = -40.1f;
-	_vertices[3].z = 40.f;
-	_vertices[3].r = 1.f;
-	_vertices[3].g = 1.f;
-	_vertices[3].b = 1.f;
-	_vertices[3].a = 1.f;
+	//_vertices[3].x = -40.1f;
+	//_vertices[3].y = -40.1f;
+	//_vertices[3].z = 40.f;
+	//_vertices[3].r = 1.f;
+	//_vertices[3].g = 1.f;
+	//_vertices[3].b = 1.f;
+	//_vertices[3].a = 1.f;
 
 
-	_indices.resize(6);
+	//_indices.resize(6);
 
-	_indices[0] = 0;
-	_indices[1] = 1;
-	_indices[2] = 2;
-	_indices[3] = 2;
-	_indices[4] = 3;
-	_indices[5] = 0;
+	//_indices[0] = 0;
+	//_indices[1] = 1;
+	//_indices[2] = 2;
+	//_indices[3] = 2;
+	//_indices[4] = 3;
+	//_indices[5] = 0;
 
-	_vertexBuffer = new VertexBuffer<FVertexSimple>(device);
+	/*_vertexBuffer = new VertexBuffer<FVertexSimple>(device);
 	_vertexBuffer->Create(_vertices);
 
 	_indexBuffer = new IndexBuffer(device);
-	_indexBuffer->Create(_indices);
+	_indexBuffer->Create(_indices);*/
 
 	_vertexShader = new VertexShader(device);
 	_vertexShader->Create(L"Shader.hlsl", "VS", "vs_5_0");
@@ -81,6 +81,9 @@ GameObject::~GameObject() {
 }
 
 void GameObject::Render() {
+	if (_vertexBuffer == nullptr)
+		return;
+
 	_deviceContext->IASetInputLayout(_inputLayout->Get());
 	_deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 	_deviceContext->VSSetShader(_vertexShader->Get(), nullptr, 0);
